30c074465ea3293525bff238ee10230fa81b75b5
Damanpreet Kaur <daman7294@gmail.com>
2019-01-01 21:49:08 -0800

    Hackerrank Linked List Solution

---
+++ LLReverse.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+# Complete the reversePrint function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reversePrint(head):
+    prev_node = curr_node = head
+    if head:
+        if curr_node.next is not None:
+            prev_node = None
+            next_node = None
+            while curr_node:
+                next_node = curr_node.next
+                curr_node.next = prev_node
+                prev_node = curr_node
+                curr_node = next_node
+        head = prev_node
+        ptr = head
+        while ptr:
+            print(ptr.data)
+            ptr = ptr.next
+
+
+if __name__ == '__main__':
